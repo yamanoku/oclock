a7b8ed6867716224ddd75bb493773c5f09d87816
yamanoku <0910yama@gmail.com>
2022-10-15 21:14:13 +0900

    mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,6 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
+import child_process from "child_process";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,7 +20,7 @@ function serve() {
   return {
     writeBundle() {
       if (server) return;
-      server = require("child_process").spawn(
+      server = child_process.spawn(
         "npm",
         ["run", "start", "--", "--dev"],
         {
