1c63c697e919e49c364f0254117e913ee6bd4e68
yamanoku <0910yama@gmail.com>
2024-03-25 17:36:00 +0900

    Vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        seconds: resolve(__dirname, 'seconds.html')
+      },
+    },
+  },
+  plugins: [svelte({
+    compilerOptions: {
+      customElement: true,
+    },
+  })],
+})
